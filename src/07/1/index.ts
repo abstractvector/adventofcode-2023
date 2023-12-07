e160d9489fe2a1b853caa50d0015fface112104d
Matt Knight <matthew.knight@campspot.com>
2023-12-07 03:28:18 -0500

    Day 7, challenge 1

---
+++ src/07/1/index.ts
@@ -0,0 +1,79 @@
+import { readFile } from 'node:fs/promises';
+
+// path to puzzle input from the elves
+// const filename = './src/07/1/example.txt';
+const filename = './src/07/1/puzzle-input.txt';
+
+// read the file into a string
+const file = await readFile(filename, 'utf-8');
+
+// split the file into lines
+const lines = file.split('\n').filter(Boolean);
+
+const cardScores = [
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  'T',
+  'J',
+  'Q',
+  'K',
+  'A',
+];
+
+const getHandType = (cards: string[]) => {
+  const cardCount: Record<string, number> = {};
+  cards.forEach((card) => {
+    if (!cardCount[card]) {
+      cardCount[card] = 1;
+    } else {
+      cardCount[card] += 1;
+    }
+  });
+
+  const uniqueCards = Object.values(cardCount).sort().reverse().join('');
+
+  if (uniqueCards === '5') return 7; // five of a kind
+  if (uniqueCards === '41') return 6; // four of a kind
+  if (uniqueCards === '32') return 5; // full house
+  if (uniqueCards === '311') return 4; // three of a kind
+  if (uniqueCards === '221') return 3; // two pair
+  if (uniqueCards === '2111') return 2; // one pair
+  if (uniqueCards === '11111') return 1; // high card
+
+  throw new Error(`Unrecognized hand`);
+};
+
+const hands = lines
+  .map((line) => {
+    const match = line.match(/(.+?)\s+(.+)/);
+
+    return {
+      cards: match[1].split(''),
+      cardStrength: match[1].split('').map((card) => cardScores.indexOf(card)),
+      type: getHandType(match[1].split('')),
+      bid: +match[2],
+    };
+  })
+  .sort((a, b) => {
+    if (a.type !== b.type) return a.type - b.type;
+    for (let i = 0; i < 5; i += 1) {
+      if (a.cardStrength[i] !== b.cardStrength[i])
+        return a.cardStrength[i] - b.cardStrength[i];
+    }
+    return 0;
+  })
+  .map((card, ix) => ({
+    ...card,
+    rank: ix + 1,
+    winnings: card.bid * (ix + 1),
+  }));
+
+const winnings = hands.reduce((a, b) => a + b.winnings, 0);
+
+console.log(`What are the total winnings? ${winnings}`);
